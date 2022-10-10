5e2927314095fbf86c1cc0442c33694254207afc
Canghosto <can.bagdas@hotmail.de>
2022-10-10 22:53:35 +0200

    Adding second and part of third course

--- Aufgabe/2/basics-assignment-4-problem/app.js
+++ Aufgabe/2/basics-assignment-4-problem/app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data(){
         return {
             user: '',
-            visibility: true
+            visibility: true,
+            inputColor: ''
         };
     },
 
